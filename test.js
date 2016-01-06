cecad654ca474e3de0b1ddafedd5289f92197f02
lilEzek <devezek@gmail.com>
2016-01-06 21:00:21 +0100

    Ahora travis tiene unos test unitarios correctos.

--- test.js
+++ test.js
@@ -5,14 +5,29 @@ var zl = require('./compilador')({
 var chai = require('chai');
 var expect = chai.expect;
 var assert = chai.assert;
+var fs = require('fs');
 
 var codigos = {
   '1': ""
 };
 
 for (var k in codigos) {
-
+  codigos[k] = fs.readFileSync("pruebas/"+k+".zl").toString();
 }
+
+describe('Forzando errores', function() {
+  it("Código mal escrito", function() {
+    expect(zl.Compilar.bind(zl,"Subrutima Ayylmao")).to.throw();
+  });
+
+  it("Variable sin declarar", function() {
+    try {
+      zl.Compilar("Subrutina B Datos a es Numero Algoritmo c <- a Fin");
+    } catch (e) {
+      expect(e.tipo).to.equal(zl.error.E_NOMBRE_NO_DEFINIDO);
+    }
+  });
+})
 
 describe('Compilando las pruebas', function() {
   for (var k in codigos) {
